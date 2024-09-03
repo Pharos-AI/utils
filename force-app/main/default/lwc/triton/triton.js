f0d03d5830786d62f80add59749c9b0baac8a816
Nikita Prokopev <glyuk@Nikitas-MacBook-Pro.local>
2024-09-02 23:14:14 -0700

    License Info take 2

--- force-app/main/default/lwc/triton/triton.js
+++ force-app/main/default/lwc/triton/triton.js
@@ -1,3 +1,15 @@
+/**
+ * Copyright (C) 2024 Pharos AI, Inc.
+ *
+ * This file is part of Pharos Triton.
+ *
+ * Pharos Triton is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * /
+
 import {LightningElement, api} from 'lwc';
 import {makeBuilder} from 'c/tritonBuilder';
 import saveComponentLogs from '@salesforce/apex/TritonLwc.saveComponentLogs';
